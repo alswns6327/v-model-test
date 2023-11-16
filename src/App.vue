bb744865d8378c6893d6163f366d18c027d71e35
owner <alswns6327@naver.com>
2023-11-16 17:11:40 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <InputName v-model:name="parentName"/>
+  {{ parentName }}
+</template>
+
+<script>
+import InputName from './components/InputName.vue';
+
+  export default{
+    name: "App",
+    data(){
+      return{
+        parentName : ""
+      }
+    },
+    components: { InputName }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/InputName.vue
@@ -0,0 +1,9 @@
+<template>
+    <input @input="$emit('update:name', $event.target.value)" :value="name"/>
+</template>
+<script>
+    export default{
+        name : "InputName",
+        props : ["name"]
+    }
+</script>
